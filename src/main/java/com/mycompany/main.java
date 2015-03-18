3124d3d87de9f47d856017053384f672c3df1b87
Lukas Pfannschmidt <mirek.1337@gmail.com>
2015-03-18 22:53:39 +0100

    last commit

--- src/main/java/com/mycompany/main.java
+++ src/main/java/com/mycompany/main.java
@@ -32,50 +32,28 @@ public class main {
     public static int N;
 
     public static void main(String[] args) throws IOException {
-        String path = "C:\\Users\\mirek_000\\Documents\\Dropbox\\workspace\\Clustering_Project\\GEOdata_Cholesteatom_nurLogRatio.csv";
+//        String path = "C:\\Users\\mirek_000\\Documents\\Dropbox\\workspace\\Clustering_Project\\GEOdata_Cholesteatom_nurLogRatio.csv";
 //        String path = "C:\\Users\\mirek_000\\Documents\\Dropbox\\workspace\\Clustering_Project\\Test.csv";
+        String path = "/home/lukas/workspace/kmeans_clustering_opencl/GEOdata_Cholesteatom_nurLogRatio.csv";
         importCSV(path);
 
-        // Select Random Prototypes from Data
-        Random random = new Random();
-        float[] prototypes = new float[DIM *K];
-        int[] prototypeIDs = new int[K];
-        for (int k = 0; k < K; k++) {
-            // Select Random Unique Indices for all K
-            boolean randomNumberUnique = true;
-                do {
-                    int index = random.nextInt(N);
-                    prototypeIDs[k] = index;
-                    for (int j = 0; j < k; j++) {
-                        if (prototypeIDs[j] == index) {
-                            randomNumberUnique = false;
-                        }
-                    }
-                } while (!randomNumberUnique);
-            // Copy Raw Values into Float Array
-            for (int d = 0; d < DIM; d++) {
-                prototypes[k* DIM +d] = data[prototypeIDs[k]+d];
-            }
-        }
-        ///
 
-        int[] nearestPrototypeforVector = new int[N];
+        long t0 = System.currentTimeMillis();
         CLContext context = JavaCL.createBestContext();
         CLQueue queue = context.createDefaultQueue();
 
         CLBuffer<Float> vectors = context.createFloatBuffer(Usage.Input, FloatBuffer.wrap(data), true);
+        float[] prototypes = initPrototypes();
         CLBuffer<Float> prototypeBuffer = context.createFloatBuffer(Usage.Input, FloatBuffer.wrap(prototypes), false);
-        CLBuffer<Integer> proto_Assignment = context.createIntBuffer(Usage.InputOutput, IntBuffer.wrap(nearestPrototypeforVector), false);
+        CLBuffer<Integer> proto_Assignment = context.createIntBuffer(Usage.Output, IntBuffer.wrap(new int[N]), false);
 
 //        // Read the program sources and compile them :
 //        String src = IOUtils.readText(com.mycompany.main.class.getResource("TutorialKernels.cl"));
 //        CLProgram program = context.createProgram(src);
 //        // Get and call the kernel :
 //        CLKernel addFloatsKernel = program.createKernel("find_nearest_prototype");
-        long t0 = System.currentTimeMillis();
 
         TutorialKernels kernels = new TutorialKernels(context);
-        int[] globalSizes = new int[] {N};
         float[] newprototypes;
         CLEvent findNearestPrototypes;
         Pointer<Integer> outPtr;
@@ -83,8 +61,9 @@ public class main {
         int[] new_selectedPrototypes;
         boolean finished = false;
         while (!finished) {
-            findNearestPrototypes = kernels.find_nearest_prototype(queue, vectors, prototypeBuffer, proto_Assignment, DIM, K, N, globalSizes, null);
+            findNearestPrototypes = kernels.find_nearest_prototype(queue, vectors, prototypeBuffer, proto_Assignment, DIM, K, N, new int[] {N}, null);
             outPtr = proto_Assignment.read(queue, findNearestPrototypes);
+
             new_selectedPrototypes = outPtr.getInts();
             // Convergence if no assignments changed
             if (Arrays.equals(new_selectedPrototypes, selectedPrototypes)) {
@@ -120,7 +99,7 @@ public class main {
             prototypeBuffer.release();
             prototypeBuffer = context.createFloatBuffer(Usage.Input, FloatBuffer.wrap(prototypes), false);
             proto_Assignment.release();
-            proto_Assignment = context.createIntBuffer(Usage.Output, IntBuffer.wrap(nearestPrototypeforVector), false);
+            proto_Assignment = context.createIntBuffer(Usage.Output, IntBuffer.wrap(new int[N]), false);
 
 //            System.out.print(new_selectedPrototypes[0] + " ");
 //            System.out.println(prototypes[new_selectedPrototypes[0]]+"x "+ prototypes[new_selectedPrototypes[0]+1]+"y");
@@ -129,6 +108,32 @@ public class main {
         long t1 = System.currentTimeMillis();
         System.out.println(t1 - t0 + "ms");
 
+    }
+
+    private static float[] initPrototypes() {
+        // Select Random Prototypes from Data
+        Random random = new Random();
+        float[] prototypes = new float[DIM *K];
+        int[] prototypeIDs = new int[K];
+        for (int k = 0; k < K; k++) {
+            // Select Random Unique Indices for all K
+            boolean randomNumberUnique = true;
+                do {
+                    int index = random.nextInt(N);
+                    prototypeIDs[k] = index;
+                    for (int j = 0; j < k; j++) {
+                        if (prototypeIDs[j] == index) {
+                            randomNumberUnique = false;
+                        }
+                    }
+                } while (!randomNumberUnique);
+            // Copy Raw Values into Float Array
+            for (int d = 0; d < DIM; d++) {
+                prototypes[k* DIM +d] = data[prototypeIDs[k]+d];
+            }
+        }
+        ///
+        return prototypes;
     }
 
 
