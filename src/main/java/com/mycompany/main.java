ead046d6171f3b3c83e0ff7f4151054d7b25d5fc
Lukas Pfannschmidt <mirek.1337@gmail.com>
2015-03-18 15:38:45 +0100

    Weiter verbessert

--- src/main/java/com/mycompany/main.java
+++ src/main/java/com/mycompany/main.java
@@ -2,6 +2,8 @@ package com.mycompany;
 
 import com.nativelibs4java.opencl.*;
 import com.nativelibs4java.opencl.CLMem.Usage;
+import com.nativelibs4java.opencl.util.OpenCLType;
+import com.nativelibs4java.opencl.util.ReductionUtils;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.bridj.Pointer;
@@ -15,20 +17,26 @@ import java.nio.IntBuffer;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.groupingByConcurrent;
 
 public class main {
-    private static final int MAX_ITERATIONS = 100;
+    private static final int MAX_ITERATIONS = 1000;
     static float[] data;
     static ArrayList<String> ids;
     public static final int K = 2;
     public static int DIM;
     public static int N;
-    public static int[] nearestPrototypeforVector;
 
     public static void main(String[] args) throws IOException {
-        String path = "C:\\Users\\mirek_000\\Documents\\Dropbox\\workspace\\Clustering_Project\\GEOdata_Cholesteatom_nurLogRatio.csv";
+//        String path = "C:\\Users\\mirek_000\\Documents\\Dropbox\\workspace\\Clustering_Project\\GEOdata_Cholesteatom_nurLogRatio.csv";
+        String path = "C:\\Users\\mirek_000\\Documents\\Dropbox\\workspace\\Clustering_Project\\Test.csv";
         importCSV(path);
 
+        // Select Random Prototypes from Data
         Random random = new Random();
         float[] prototypes = new float[DIM *K];
         int[] prototypeIDs = new int[K];
@@ -49,14 +57,15 @@ public class main {
                 prototypes[k* DIM +d] = data[prototypeIDs[k]+d];
             }
         }
+        ///
 
-        nearestPrototypeforVector = new int[N];
+        int[] nearestPrototypeforVector = new int[N];
         CLContext context = JavaCL.createBestContext();
         CLQueue queue = context.createDefaultQueue();
 
         CLBuffer<Float> vectors = context.createFloatBuffer(Usage.Input, FloatBuffer.wrap(data), true);
-        CLBuffer<Float> prototypeBuffer = context.createFloatBuffer(Usage.Input, FloatBuffer.wrap(prototypes), true);
-        CLBuffer<Integer> proto_Assignment = context.createIntBuffer(Usage.Output, IntBuffer.wrap(nearestPrototypeforVector), true);
+        CLBuffer<Float> prototypeBuffer = context.createFloatBuffer(Usage.Input, FloatBuffer.wrap(prototypes), false);
+        CLBuffer<Integer> proto_Assignment = context.createIntBuffer(Usage.Output, IntBuffer.wrap(nearestPrototypeforVector), false);
 
 //        // Read the program sources and compile them :
 //        String src = IOUtils.readText(com.mycompany.main.class.getResource("TutorialKernels.cl"));
@@ -67,35 +76,39 @@ public class main {
 
         TutorialKernels kernels = new TutorialKernels(context);
         int[] globalSizes = new int[] {N};
-
+        float[] newprototypes;
+        CLEvent findNearestPrototypes;
+        Pointer<Integer> outPtr;
+        int[] selectedPrototypes;
         for (int t = 0; t < MAX_ITERATIONS; t++) {
-            CLEvent findNearestPrototypes;
-            int[] selectedPrototypes;
-            Pointer<Integer> outPtr;
             findNearestPrototypes = kernels.find_nearest_prototype(queue, vectors, prototypeBuffer, proto_Assignment, DIM, K, N, globalSizes, null);
 
             outPtr = proto_Assignment.read(queue, findNearestPrototypes);
             selectedPrototypes = outPtr.getInts();
-            float[] newprototypes = new float[DIM * K];
+            newprototypes = new float[DIM * K];
             for (int i = 0; i < N; i++) {
                 for (int d = 0; d < DIM; d++) {
-                    newprototypes[selectedPrototypes[i]* DIM +d] += data[i+d];
+                    newprototypes[selectedPrototypes[i] * DIM + d] += data[i + d];
                 }
             }
             for (int k = 0; k < K; k++) {
                 final int filter = k;
                 long count= Arrays.stream(selectedPrototypes).parallel().filter((s->s == filter)).count();
+                System.out.print("count:"+count + " ");
                 for (int d = 0; d < DIM; d++) {
                     newprototypes[k* DIM +d] /= count;
                 }
             }
+
             prototypes = newprototypes;
-            prototypeBuffer = context.createFloatBuffer(Usage.Input, FloatBuffer.wrap(prototypes), true);
-            proto_Assignment = context.createIntBuffer(Usage.Output, IntBuffer.wrap(nearestPrototypeforVector), true);
+            prototypeBuffer.release();
+            prototypeBuffer = context.createFloatBuffer(Usage.Input, FloatBuffer.wrap(prototypes), false);
+            proto_Assignment.release();
+            proto_Assignment = context.createIntBuffer(Usage.Output, IntBuffer.wrap(nearestPrototypeforVector), false);
 
-
+            System.out.print(selectedPrototypes[0] + " ");
+            System.out.println(prototypes[selectedPrototypes[0]]+"x "+ prototypes[selectedPrototypes[0]+1]+"y");
         }
-
         long t1 = System.currentTimeMillis();
         System.out.println(t1 - t0 + "ms");
 
@@ -106,7 +119,7 @@ public class main {
 
         try {
             Reader in = new FileReader(path);
-            List<CSVRecord> records = CSVFormat.TDF.parse(in).getRecords();  // Tab Delimiter
+            List<CSVRecord> records = CSVFormat.DEFAULT.parse(in).getRecords();  // Tab Delimiter
 
             DIM = records.get(0).size()-1;   // -1 ID column
             N = records.size()-1;     // -1 Header Row
@@ -118,18 +131,19 @@ public class main {
                 CSVRecord record = records.get(j);
                 ids.add(record.get(0));
 
-                for (int i = 1; i < DIM; i++) {
+                for (int i = 1; i < DIM+1; i++) {
                     data[(j-1)* DIM + i-1]= nf.parse(record.get(i)).floatValue();
                 }
 
             }
-            System.out.println();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ParseException e) {
             e.printStackTrace();
+        } finally {
+
         }
     }
 }
